808a92eac32223e85ca5c5838a51b9d79f0312fb
熊壮(003357 <xiongzhuang@fcbox.com>
2019-12-19 17:28:37 +0800

    参数加密

--- lib/data_helper.dart
+++ lib/data_helper.dart
@@ -15,6 +15,21 @@ class DataHelper{
     return map;
   }
 
+  static encryptParams(Map<String, dynamic> map) {
+    var buffer = StringBuffer();
+    map.forEach((key, value) {
+      buffer.write(key);
+      buffer.write(value);
+    });
+
+    buffer.write("SERECT");
+    print('bustring--->:' + buffer.toString());
+    var sign = string2MD5(buffer.toString());
+    map["sign"] = sign;
+    print("sign--->" + sign);
+    return map;
+  }
+
   static string2MD5(String data) {
     var content = new Utf8Encoder().convert(data);
     var digest = md5.convert(content);
